a87bbc0b58b551eeecdff41ec7e139f65715da61
MAL <kinmal1988@gmail.com>
2023-06-24 18:56:33 +0300

    progr HW_5

--- Программист/Java/HW_5.java
+++ Программист/Java/HW_5.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.jar.Attributes.Name;
+
 
 public class HW_5 {
     public static void main(String[] args) {
